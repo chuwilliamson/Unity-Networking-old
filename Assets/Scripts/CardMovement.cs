10d89ea72ba00ef5cd0ecb0ef0c3ede91f0790ca
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-04-07 16:02:33 -0500

    card flipping

---
+++ Assets/Scripts/CardMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMovement : MonoBehaviour
+{
+    bool IsFlipped = false;
+
+    protected virtual IEnumerator CardFlip()
+    {
+        Quaternion TargetRotation = new Quaternion();
+        if (!IsFlipped)
+        {
+            TargetRotation = new Quaternion(0, 0, 1, 0);
+            Debug.Log(TargetRotation);
+        }
+        else
+        {
+            TargetRotation = new Quaternion(0, 0, 0, 0);
+            Debug.Log(TargetRotation);
+            Debug.Log(transform.rotation);
+        }
+        while (transform.rotation.z != TargetRotation.z && transform.rotation.w != TargetRotation.w)
+        {
+            Debug.Log(transform.rotation);
+            Quaternion rotation = transform.rotation;
+            rotation.z += Time.deltaTime * 1;
+            transform.rotation = rotation;
+            if (transform.rotation.z >= .8 && transform.rotation.w <= .4)
+                transform.rotation = TargetRotation;
+            yield return null;
+        }
+        IsFlipped = !IsFlipped;
+        StopCoroutine(CardFlip());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(CardFlip());
+
+        }
+
+    }
+}
