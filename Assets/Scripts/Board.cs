95be8279e3c540542be8fc69e4fc31eaaef10262
chuwilliamson <chuwilliamson@gmail.com>
2016-04-07 12:23:21 -0500

    static method to get the board properties

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,17 +3,44 @@ using System.Collections;
 
 namespace Matthew
 {
+	///EXAMPLE: Transform mysteryPos = Board.Instance.MysteryCardStackPosition;
+	/// <summary>
+	/// Board.
+	/// </summary>
 	public class Board : MonoBehaviour
 	{
+		private static Board m_instance;
+
+		/// <summary>
+		/// Gets the instance.
+		/// </summary>
+		/// <value>The instance.</value>
+		public static Board Instance
+		{
+			get
+			{
+				if (m_instance == null)
+					m_instance = FindObjectOfType<Board>();
+				return m_instance;
+			}
+		}
 		[SerializeField]
 		private GameObject m_MysteryCardStack;
 		[SerializeField]
 		private GameObject m_TreasureCardStack;
 
-		public Transform MysterCardStackPosition
+		/// <summary>
+		/// Gets the mystery card stack position.
+		/// </summary>
+		/// <value>The mystery card stack position.</value>
+		public Transform MysteryCardStackPosition
 		{
 			get{return m_MysteryCardStack.transform;}
 		}
+		/// <summary>
+		/// Gets the m treasure card transform.
+		/// </summary>
+		/// <value>The m treasure card transform.</value>
 		public Transform m_TreasureCardTransform
 		{
 			get{ return m_TreasureCardStack.transform; }
