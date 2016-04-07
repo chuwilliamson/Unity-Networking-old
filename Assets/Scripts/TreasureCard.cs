2677b65ef84cdf0d8a69afd8af6a82edacb34034
chuwilliamson <chuwilliamson@gmail.com>
2016-04-07 13:54:44 -0500

    dylan

---
+++ Assets/Scripts/TreasureCard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Dylan;
+
+public class TreasureCard : Card, iCard
+{
+	protected string ItemSlot;
+	protected int GoldValue;
+
+	public string name { get { return Name; } set { Name = value; } }
+
+	public bool State { get { return InPlay; } set { InPlay = value; } }
+
+	string iCard.Description { 
+		get { 
+			return Description; 
+		} 
+		set { 
+			Description = value; 
+		} 
+	}
+}
