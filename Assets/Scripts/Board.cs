52940e46e1afa1fe9648a99d3d9029ec2afacf01
chuwilliamson <chuwilliamson@gmail.com>
2016-04-07 12:16:53 -0500

    board class for accessing the board properties

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Matthew
+{
+	public class Board : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject m_MysteryCardStack;
+		[SerializeField]
+		private GameObject m_TreasureCardStack;
+
+		public Transform MysterCardStackPosition
+		{
+			get{return m_MysteryCardStack.transform;}
+		}
+		public Transform m_TreasureCardTransform
+		{
+			get{ return m_TreasureCardStack.transform; }
+		}
+		private void Start()
+		{
+			
+		}
+	}
+
+}
