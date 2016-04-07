e9847a9b3a271e7d221bb39590dc4076a8f3b970
Eric <EricMouledoux@Gmail.com>
2016-04-07 13:45:30 -0500

    removed unused namespaces in iPlayer

--- Assets/Scripts/iPlayer.cs
+++ Assets/Scripts/iPlayer.cs
@@ -1,10 +1,8 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace Eric
+﻿namespace Eric
 {
     public interface iPlayer
     {
+        int PlayCard();
         int GainGold(int a_gold);
         int LevelUp(int a_levels);
         int GainExperience(int a_experience);
