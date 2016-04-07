81790523e099a30a25198cb8975ba91bca00ef67
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-04-07 17:56:35 -0500

    Flip cards on mouse click

---
+++ Assets/Scripts/CardMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMovement : MonoBehaviour
+{
+    public bool IsFlipped = false;
+
+    void Update()
+    {
+        //Debug.Log(transform.rotation);
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (IsFlipped == false)
+            {
+                transform.rotation = new Quaternion(0, 0, 1, 0);
+                IsFlipped = true;
+            }
+            else if (IsFlipped == true)
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 1);
+                IsFlipped = false;
+            }
+        }
+
+        CardFlip();
+    }
+
+    void CardFlip()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 30) && hit.collider.GetComponent<CardMovement>() != null)
+            {
+                if (IsFlipped == false)
+                {
+                    transform.rotation = new Quaternion(0, 0, 1, 0);
+                    IsFlipped = true;
+                }
+                else if (IsFlipped == true)
+                {
+                    transform.rotation = new Quaternion(0, 0, 0, 1);
+                    IsFlipped = false;
+                }
+            }
+        }
+    }
+
+}
