8345bcbbb252d453ad83429a00a5c39237eb762f
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-04-07 13:41:51 -0500

    MysteryCard and TressureCard

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -9,8 +9,6 @@ namespace Dylan
         string name { get; set; }
         string Description { get; set; }
         bool State { get; set; }
-
-        void Active();
     }
 
     public class Card
@@ -21,6 +19,8 @@ namespace Dylan
         protected string Description; //Effect the card has when played on field
 
         protected bool InPlay = false; //Used to tag the card as on the Playing field or in a player hand
+
+
     }
 }
 
