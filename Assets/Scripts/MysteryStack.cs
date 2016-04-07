4a45682796d402d25568f8c5a2f6b86c83e21e35
chuwilliamson <chuwilliamson@gmail.com>
2016-04-07 17:02:35 -0500

    push and pull stack elements

--- Assets/Scripts/MysteryStack.cs
+++ Assets/Scripts/MysteryStack.cs
@@ -1,9 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
-public class MysteryStack : MonoBehaviour {
-	public List<TreasureCardMono> MysteryCards;
-	public Stack<TreasureCardMono> ls;
+public class MysteryStack : MonoBehaviour
+{
+    public List<MysteryCardMono> MysteryCards;
+    private static List<MysteryCardMono> m_cards;
+    private static Stack<MysteryCardMono> m_stack;
+    void Start()
+    {
+        m_cards.AddRange(MysteryCards);
+        foreach (MysteryCardMono c in m_cards)
+        {
+            m_stack.Push(c);
+        }
+        
+    }
+    
+    public static MysteryCardMono Draw()
+    {
+        return m_stack.Pop();
+    }
+
+    public static void Push(MysteryCardMono card)
+    {
+        m_stack.Push(card);    
+    }
+ 
+
  
 }
--- Assets/Scripts/TreasureStack.cs
+++ Assets/Scripts/TreasureStack.cs
@@ -4,4 +4,28 @@ using System.Collections.Generic;
 public class TreasureStack : MonoBehaviour {
 	[SerializeField]
 	public List<TreasureCardMono> TreasureCards;
+
+    public List<TreasureCardMono> MysteryCards;
+    private static List<TreasureCardMono> m_cards;
+    private static Stack<TreasureCardMono> m_stack;
+    void Start()
+    {
+        m_cards.AddRange(MysteryCards);
+        foreach (TreasureCardMono c in m_cards)
+        {
+            m_stack.Push(c);
+        }
+
+    }
+
+    public static TreasureCardMono Draw()
+    {
+        return m_stack.Pop();
+    }
+
+    public static void Push(TreasureCardMono card)
+    {
+        m_stack.Push(card);
+    }
+
 }
