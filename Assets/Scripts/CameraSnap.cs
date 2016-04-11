73217b041d753e9e87c99da8dafb7c04b0bad630
Zac King <Zacharyking18@yahoo.com>
2016-04-11 16:31:01 -0500

    Camera Snap
    Basic Camera snap
    pos 25+ in the Y from the targeted location
    camera forward faces origin (0,0,0)

---
+++ Assets/Scripts/CameraSnap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSnap : MonoBehaviour
+{
+    public static void CameraSnapOverTarget(Vector3 targetPos)
+    {
+        GameObject camera = Camera.main.gameObject;
+        Vector3 DesiredCameraPosistion = targetPos + new Vector3(0, 25, 0);
+
+        camera.transform.position = DesiredCameraPosistion;
+        camera.transform.LookAt(new Vector3(0, 0, 0));
+    }
+
+    
+    //void Update()
+    //{
+    //    Debug.DrawLine(transform.position, Vector3.zero, Color.blue);
+    //}
+}
