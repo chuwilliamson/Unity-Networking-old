d5ab376e4a8a80d6a2e7b0bb92f0c0a6b4298635
Zac King <Zacharyking18@yahoo.com>
2016-04-12 10:56:12 -0500

    Fix Camera
    think this is the last

--- Assets/Scripts/CameraSnap.cs
+++ Assets/Scripts/CameraSnap.cs
@@ -7,10 +7,12 @@ public class CameraSnap : MonoBehaviour
     {
         GameObject camera = Camera.main.gameObject;
         camera.transform.parent = targetPos;
-        
-        camera.transform.position = targetPos.position +  new Vector3(0, 28f, -6f);
+
+        camera.transform.localPosition = new Vector3(0,0,0);
+        camera.transform.localPosition += new Vector3(0, 28f, -6f);
         camera.transform.parent = null;
 
+        camera.transform.rotation = new Quaternion(0,0,0,0);
         camera.transform.LookAt(new Vector3(0, 0, 0));
     }
 
