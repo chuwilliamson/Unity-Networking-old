bafc2cccc9632a340392de80fd3359795ec6db25
Eric <EricMouledoux@Gmail.com>
2016-04-13 15:45:55 -0500

    created game state manager script

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour
+{
+    public enum GAMESTATES
+    {
+        START,
+        COMBAT,
+        END,
+    }
+
+
+}
